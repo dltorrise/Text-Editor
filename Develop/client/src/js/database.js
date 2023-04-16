86a63076ff250edccf1d11491c0f216bae5781a9
dltorrise <dltorrise@gmail.com>
2023-04-15 19:32:46 -0500

    indexdb

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,10 +12,25 @@ const initdb = async () =>
     },
   });
 
+  //activities 23 & 24
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {console.error('putDb not implemented');
+const textDb = await openDB('jate', 1)
+const tx = textDb.transaction('jate', 'readwrite')
+const store = tx.objectStore('jate')
+const request = store.add({ text: content})
+const result = await request
+console.log('🚀 - data saved to the database', result);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+export const getDb = async () => {console.error('getDb not implemented');
+const textDb = await openDB('jate', 1)
+const tx = textDb.transaction('jate', 'readonly')
+const store = tx.objectStore('jate')
+const request = store.getAll()
+const result = await request
+console.log('result.value', result)
+return result 
+}
 initdb();
--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin')
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -18,12 +19,37 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new MiniCssExtractPlugin(),
+      //documentation
+      new WorkboxPlugin.GenerateSW(),
     ],
 
     module: {
       rules: [
-        
+        {
+          //css loader, style loader vs. css loader
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        //babel loader
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
